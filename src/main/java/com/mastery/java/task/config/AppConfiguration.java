cae5826fd4d20b68e3046ebb023e0c5690c67e98
sergeykon <beergood6@gmail.com>
2020-05-15 23:56:21 +0300

    add test database

--- src/main/java/com/mastery/java/task/config/AppConfiguration.java
+++ src/main/java/com/mastery/java/task/config/AppConfiguration.java
@@ -1,4 +1,23 @@
 package com.mastery.java.task.config;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class AppConfiguration {
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:~/test;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }
+
+
 }
