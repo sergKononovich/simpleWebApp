7f4f16106ba4e11a1ad189a5bf31f68765781713
sergeykon <beergood6@gmail.com>
2020-05-18 01:01:22 +0300

    add getters/setters and to String

--- src/main/java/com/mastery/java/task/dto/Employee.java
+++ src/main/java/com/mastery/java/task/dto/Employee.java
@@ -4,4 +4,40 @@ public class Employee {
     private Long employeeId;
     private String firstName;
     private Gender gender;
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public Employee setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Employee setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public Employee setGender(Gender gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
 }
