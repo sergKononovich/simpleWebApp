7b79e5c5b63dbd6767c64ca05fedf850b6a2ec1e
sergeykon <beergood6@gmail.com>
2020-05-17 20:08:57 +0300

    spring-boot starter application

--- src/main/java/com/mastery/java/task/config/AppConfiguration.java
+++ src/main/java/com/mastery/java/task/config/AppConfiguration.java
@@ -1,23 +1,8 @@
 package com.mastery.java.task.config;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfiguration {
-
-    @Bean
-    public DataSource getDataSource() {
-        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:~/test;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE");
-        dataSourceBuilder.username("SA");
-        dataSourceBuilder.password("");
-        return dataSourceBuilder.build();
-    }
-
-
 }
