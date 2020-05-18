077bb962106fe788f5bb0c3bdf499212aa2bffa0
sergeykon <beergood6@gmail.com>
2020-05-18 23:57:46 +0300

    fix sql and tests

--- src/main/resources/data-h2.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO employee (first_name, gender) VALUES
-  ('Bob', 'MALE'),
-  ('Bill', 'MALE'),
-  ('Margo', 'FEMALE');
--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -1,7 +1,11 @@
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE employee (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
+  id BIGINT AUTO_INCREMENT  PRIMARY KEY,
   first_name VARCHAR(250) NOT NULL,
+  last_name VARCHAR(250) NOT NULL,
+  department_id INT NOT NULL,
+  job_title TEXT NOT NULL,
   gender VARCHAR(250) NOT NULL,
+  birthday DATE NOT NULL
 );
