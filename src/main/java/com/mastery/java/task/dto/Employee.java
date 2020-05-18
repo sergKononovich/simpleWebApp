14c20f63491261bfb4d4902292ca0b7ff40d2847
sergeykon <beergood6@gmail.com>
2020-05-18 23:57:14 +0300

    add fields to Employee dto

--- src/main/java/com/mastery/java/task/dto/Employee.java
+++ src/main/java/com/mastery/java/task/dto/Employee.java
@@ -1,60 +1,16 @@
 package com.mastery.java.task.dto;
 
-import java.util.Objects;
+import lombok.Data;
 
+import java.time.LocalDate;
+
+@Data
 public class Employee {
     private Long employeeId;
     private String firstName;
+    private String lastName;
+    private Long departmentId;
+    private String jobTitle;
     private Gender gender;
-
-    public Long getEmployeeId() {
-        return employeeId;
-    }
-
-    public Employee setEmployeeId(Long employeeId) {
-        this.employeeId = employeeId;
-        return this;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public Employee setFirstName(String firstName) {
-        this.firstName = firstName;
-        return this;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public Employee setGender(Gender gender) {
-        this.gender = gender;
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Employee employee = (Employee) o;
-        return Objects.equals(employeeId, employee.employeeId) &&
-                Objects.equals(firstName, employee.firstName) &&
-                gender == employee.gender;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(employeeId, firstName, gender);
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "employeeId=" + employeeId +
-                ", firstName='" + firstName + '\'' +
-                ", gender=" + gender +
-                '}';
-    }
+    private LocalDate birthday;
 }
