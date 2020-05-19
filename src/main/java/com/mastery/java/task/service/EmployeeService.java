1ece8c1364703228e34ee5230642e9807f28910e
sergeykon <beergood6@gmail.com>
2020-05-19 20:37:30 +0300

    Service implementation.

--- src/main/java/com/mastery/java/task/service/EmployeeService.java
+++ src/main/java/com/mastery/java/task/service/EmployeeService.java
@@ -1,4 +1,46 @@
 package com.mastery.java.task.service;
 
-public class EmployeeService {
+import com.mastery.java.task.dto.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Service interface for business logic.
+ */
+public interface EmployeeService {
+
+    /**
+     * Persist new employee.
+     * @param employee Employee.
+     * @return Employee id.
+     */
+    Long create(Employee employee);
+
+    /**
+     * Get all employee.
+     * @return Employee list.
+     */
+    List<Employee> getAll();
+
+    /**
+     * Get employee by id.
+     * @param id employee id.
+     * @return Employee.
+     */
+    Optional<Employee> getById(Long id);
+
+    /**
+     * Update employee.
+     * @param employee Employee.
+     * @return number of updated records.
+     */
+    Integer update(Employee employee);
+
+    /**
+     * Delete employee by id.
+     * @param id id.
+     * @return number of updated records.
+     */
+    Integer delete(Long id);
 }
---
+++ src/main/java/com/mastery/java/task/service/EmployeeServiceImpl.java
@@ -0,0 +1,88 @@
+package com.mastery.java.task.service;
+
+import com.mastery.java.task.dao.EmployeeDao;
+import com.mastery.java.task.dto.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Employee service implementation
+ */
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    /**
+     * Logger.
+     */
+    private Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+
+    /**
+     * EmployeeDao instance.
+     */
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    /**
+     * {@inheritDoc}
+     * @param employee Employee.
+     * @return Employee id.
+     */
+    @Override
+    public Long create(Employee employee) {
+        LOGGER.debug("create ({})", employee);
+
+        return employeeDao.create(employee);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return Employee list.
+     */
+    @Override
+    public List<Employee> getAll() {
+        LOGGER.debug("getAll");
+
+        return employeeDao.getAll();
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param id employee id.
+     * @return Employee wrapped in Optional.
+     */
+    @Override
+    public Optional<Employee> getById(Long id) {
+        LOGGER.debug("getById ({})", id);
+
+        return employeeDao.getById(id);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param employee Employee.
+     * @return number of updated records.
+     */
+    @Override
+    public Integer update(Employee employee) {
+        LOGGER.debug("update ({})", employee);
+
+        return employeeDao.update(employee);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param id id.
+     * @return number of deleted records.
+     */
+    @Override
+    public Integer delete(Long id) {
+        LOGGER.debug("delete ({})", id);
+
+        return employeeDao.delete(id);
+    }
+}
