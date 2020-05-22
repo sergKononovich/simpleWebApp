a9d5fd23e5e63fab8a0ff851bfe59bbeb541142d
sergeykon <beergood6@gmail.com>
2020-05-22 14:27:11 +0300

    Refactor

--- src/main/java/com/mastery/java/task/config/AppConfiguration.java
+++ src/main/java/com/mastery/java/task/config/AppConfiguration.java
@@ -1,13 +1,29 @@
 package com.mastery.java.task.config;
 
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Arrays;
 
 @Configuration
-@PropertySource("classpath:application.properties")
-@PropertySource("classpath:dao.properties")
+@PropertySource({"classpath:application.properties", "classpath:dao.properties"})
 public class AppConfiguration {
 
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
+    }
 }
